7501d51f77a2852f0f4bed0d6fc467cf8b39beb7
tingli1991 <litingxian@live.cn>
2018-08-12 22:54:41 +0800

    提交Log4Less日志记录扩展类

--- Exceptionless/src/Exceptionless.WebAPITest/Controllers/ValuesController.cs
+++ Exceptionless/src/Exceptionless.WebAPITest/Controllers/ValuesController.cs
@@ -6,22 +6,38 @@ namespace Exceptionless.WebAPITest.Controllers
     /// <summary>
     /// 
     /// </summary>
-    [Route("api/[controller]"), ApiController]
+    [Route("api/Values"), ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<string> Get()
+        /// <summary>
+        ///  GET api/values/error
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Error")]
+        public ActionResult<string> Error()
         {
             try
             {
-                throw new Exception("ExceptionDemo 的异常");
+                throw new Exception($"Exceptionless异常测试：{DateTime.Now.ToString("yyyyMMddHHmmss")}");
             }
             catch (Exception ex)
             {
-                //核心代码：向Exceptionless提交异常信息
-                ex.ToExceptionless().Submit();
+                //核心代码：向Exceptionless提交异常信息,下面三种写法相同
+                //ex.ToExceptionless().Submit();//原生写法
+                //Log4Less.Submit(ex, new { Id = 1, Name = "张三" }, "异常扩展方法测试");//扩展写法
+                ex.Submit(new { Id = 1, Name = "张三" }, "异常扩展方法测试");//扩展写法(推荐使用)
             }
+            return "";
+        }
+
+        /// <summary>
+        ///  GET api/values/info
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Info")]
+        public ActionResult<string> Info()
+        {
+            Log4Less.Info($"Exceptionless信息测试：{DateTime.Now.ToString("yyyyMMddHHmmss")}", new { Id = 1, Name = "张三" }, "信息扩展方法测试");
             return "";
         }
     }
